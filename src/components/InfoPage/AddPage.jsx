9a600bc59fa0d0f50d9be04761e22363d48f5e0c
Tan Nguyen <tannguyen1024@gmail.com>
2020-05-21 14:55:28 -0500

    Updated notes

--- src/components/InfoPage/AddPage.jsx
+++ src/components/InfoPage/AddPage.jsx
@@ -4,12 +4,16 @@ import { useState } from 'react';
 
 const AddPage = () => { /* This is a FUNCTIONAL COMPONENT with an ONCHANGE EVENT using useState from 'react' */
     const [description, setDescription] = useState('');
-    // const [url, setUrl] = useState('');
+    const [url, setUrl] = useState('');
     console.log('This is our onChange description:', description);
+    console.log('This is our onChange url:', url);
+
+    /* NOTE LET'S PACKAGE UP our URL and Description and send DISPATCH for our POST  ----  DO THIS NEXT AFTER MEETING */
+
     return(
         <div>
             <input name="description" onChange={(event)=> setDescription (event.target.value)} type="text" placeholder="description of image" />
-            {/* <input name="url" onChange={(event)=> setUrl (event.target.value)}type="text" placeholder="url of image" /> */}
+            <input name="url" onChange={(event)=> setUrl (event.target.value)}type="text" placeholder="url of image" />
             <button>Add To Shelf</button>
         </div>
     )
